973ef818a36ec050101551d49a9438d8542d6329
= <=>
2024-10-30 21:08:44 -0300

    actualizacion de archivo principal

--- src/Segundo_Parcial.java
+++ src/Segundo_Parcial.java
@@ -12,13 +12,13 @@ public class Segundo_Parcial {
             System.out.println("2. Ordenamiento");
             System.out.println("3. Pila & Cola con Arreglo");
             System.out.println("4. Pila & Cola con Lista");
-            System.out.println("5. Práctico 5");
+            System.out.println("5. Árbol Binario"); // Nueva opción para el Práctico 5
             System.out.println("6. Práctico 6");
             System.out.println("7. Práctico 7");
             System.out.println("8. Práctico 8");
             System.out.println("9. Práctico 9");
             System.out.println("0. Salir");
-            System.out.print("Opcion: ");
+            System.out.print("Opción: ");
             opcion = in.nextInt();
 
             switch (opcion) {
@@ -26,8 +26,9 @@ public class Segundo_Parcial {
                 case 2 -> PracticoOrdenamiento.mostrarMenu();
                 case 3 -> PracticoColaPila.mostrarMenu();
                 case 4 -> PracticoColaPilaLista.mostrarMenu();
+                case 5 -> PracticoArbolBinario.mostrarMenu(); // Llamado al menú del Práctico 5
                 case 0 -> System.out.println("Saliendo...");
-                default -> System.out.println("Error, Ingrese otra opcion...");
+                default -> System.out.println("Error, Ingrese otra opción...");
             }
         } while (opcion != 0);
 
