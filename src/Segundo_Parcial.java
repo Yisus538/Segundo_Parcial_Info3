08bc149dcfebaf955eb7a5850ebf1485ecabf99e
= <=>
2024-11-12 21:43:36 -0300

    DONE

--- src/Segundo_Parcial.java
+++ src/Segundo_Parcial.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
 import utils.*;
+import utils.PracticoArbolRojoyNegro;
 
 public class Segundo_Parcial {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int opcion;
+        int opc;
 
         do {
             System.out.println("---- MENU ----");
-            System.out.println("1. Recursividad");
-            System.out.println("2. Ordenamiento");
+            System.out.println("1. Recursividad"); // DONE
+            System.out.println("2. Ordenamiento"); // DONE
             System.out.println("3. Pila & Cola con Arreglo");
             System.out.println("4. Pila & Cola con Lista");
             System.out.println("5. Arbol Binario");
-            System.out.println("6. Práctico 7");
-            System.out.println("7. Práctico 8");
-            System.out.println("8. Práctico 9");
+            System.out.println("6. Arbol Rojo & Negro");
+            System.out.println("7. Monticulo & Tabla Hash");
             System.out.println("0. Salir");
-            System.out.print("Opción: ");
-            opcion = in.nextInt();
+            System.out.print("Opcion: ");
+            opc = in.nextInt();
 
-            switch (opcion) {
+            switch (opc) {
                 case 1 -> PracticoRecursividad.mostrarMenu();
                 case 2 -> PracticoOrdenamiento.mostrarMenu();
                 case 3 -> PracticoColaPila.mostrarMenu();
                 case 4 -> PracticoColaPilaLista.mostrarMenu();
                 case 5 -> PracticoArbolBinario.mostrarMenu();
+                case 6 -> PracticoArbolRojoyNegro.mostrarMenu();
+                case 7-> PracticoEstructurasGenerales.mostrarMenu();
                 case 0 -> System.out.println("Saliendo...");
-                default -> System.out.println("Error, Ingrese otra opción...");
+                default -> System.out.println("Error,Ingrese otra opcion...");
             }
-        } while (opcion != 0);
+        } while (opc != 0);
 
         in.close();
     }
