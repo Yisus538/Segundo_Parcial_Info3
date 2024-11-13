8937ff18df0aa1ba7c420c8bcf176f33cacb8f82
= <=>
2024-11-13 12:22:09 -0300

    DONE

--- src/utils/PracticoArbolRojoyNegro.java
+++ src/utils/PracticoArbolRojoyNegro.java
@@ -1,43 +1,53 @@
-package utils.estructuras;
+package utils;
 
 import utils.estructuras.arbolBinario.*;
 
 import java.util.Scanner;
 
 public class PracticoArbolRojoyNegro {
-    public static void mostrarMenu() {  // Cambiado de main() a mostrarMenu()
+
+    public static void mostrarMenu() {
         ArbolRojiNegro arbol = new ArbolRojiNegro();
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         boolean salir = false;
 
         while (!salir) {
-            System.out.println("\n--- MENÚ PRÁCTICO 8 - ÁRBOL ROJINEGRO ---");
+            System.out.println("\n--- PRACTICO 8 - ARBOL ROJINEGRO ---");
             System.out.println("1. Insertar un valor");
-            System.out.println("2. Visualizar el árbol");
-            System.out.println("3. Salir");
+            System.out.println("2. Insertar valores predefinidos");
+            System.out.println("3. Visualizar el árbol");
+            System.out.println("4. Salir");
             System.out.print("Seleccione una opción: ");
 
-            int opcion = scanner.nextInt();
+            int opc = in.nextInt();
 
-            switch (opcion) {
+            switch (opc) {
                 case 1 -> {
                     System.out.print("Ingrese el valor a insertar: ");
-                    int valor = scanner.nextInt();
-                    arbol.insertar(valor);
+                    arbol.insertar(in.nextInt());
                     System.out.println("Valor insertado correctamente.");
                 }
                 case 2 -> {
-                    System.out.println("Visualización del árbol rojinegro:");
-                    arbol.visualizarArbol();
+                    insertarValoresPredefinidos(arbol);
+                    System.out.println("Valores predefinidos insertados.");
                 }
                 case 3 -> {
+                    System.out.println("Visualización del arbol rojinegro:");
+                    arbol.visualizarArbol();
+                }
+                case 4 -> {
                     salir = true;
                     System.out.println("Saliendo del menú de Árbol Rojinegro.");
                 }
-                default -> System.out.println("Opción no válida. Intente de nuevo.");
+                default -> System.out.println("Opcion no válida...");
             }
         }
-        scanner.close();
+        in.close();
+    }
+    private static void insertarValoresPredefinidos(ArbolRojiNegro arbol) {
+        int[] valores = {10, 20, 30, 40, 50, 60, 70};
+        for (int valor : valores) {
+            arbol.insertar(valor);
+        }
     }
 }
-
